fd19154a70a10703395ebb333b7616bb70128247
Lucie Fontaine <fontaluc@gmail.com>
2025-02-27 19:01:17 +0100

    Fix wrong param order in model

--- src/pcn/models.py
+++ src/pcn/models.py
@@ -452,7 +452,7 @@ class iPCModel(object):
         return self.layers
     
 class PCModule(nn.Module):
-    def __init__(self, nodes, act_fn, mu_dt, use_bias=False, kaiming_init=False):
+    def __init__(self, nodes, mu_dt, act_fn, use_bias=False, kaiming_init=False):
         super().__init__()
         self.nodes = nodes
         self.mu_dt = mu_dt
