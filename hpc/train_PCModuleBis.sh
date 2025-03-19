2daf858a6871034b01ddb53997fe5441af1fc256
Lucie Fontaine <fontaluc@gmail.com>
2025-03-19 17:50:50 +0100

    Add bash script for PModuleBis training

---
+++ hpc/train_PCModuleBis.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+### General options
+### –- specify queue --
+#SBATCH -C a100
+### -- set the job Name --
+#SBATCH -J train_PCModuleBis
+### -- set the job array --
+#SBATCH --array=1-2
+### -- ask for number of cores (default: 1) --
+#SBATCH -n 1
+### -- Select the resources: 1 gpu in exclusive process mode --
+#SBATCH --exclusive
+### -- set walltime limit: j-h:m:s
+#SBATCH --time 10:0:0
+### -- Specify the output and error file. %A_%a is the job-id --
+### -- -o and -e mean append, -oo and -eo mean overwrite --
+#SBATCH -o /home/lfontain/unsupervised-pcn/outputs/logs/train_PCModuleBis_%A_%a.out
+#SBATCH -e /home/lfontain/unsupervised-pcn/outputs/logs/train_PCModuleBis_%A_%a.err
+# -- end of Slurm options --
+
+
+# Load modules
+module load build/conda/4.10
+module load compiler/cuda/12.3
+
+conda activate torch_env
+
+N=(64 10097)
+n=${N[$SLURM_ARRAY_TASK_ID - 1]}
+N_epochs=(20000 1000)
+n_epochs=${N_epochs[$SLURM_ARRAY_TASK_ID - 1]}
+srun python $HOME/unsupervised-pcn/src/pcn/train_PCModuleBis.py --n_epochs=$n_epochs --N=$n
