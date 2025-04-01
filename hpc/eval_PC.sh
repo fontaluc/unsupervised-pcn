3e52b6a9d84d91121824c180fb39a7bcf37e9768
Lucie Fontaine <fontaluc@gmail.com>
2025-04-01 11:09:44 +0200

    Change project directory to beegfs and add more time

--- hpc/eval_PC.sh
+++ hpc/eval_PC.sh
@@ -9,11 +9,11 @@
 ### -- ask for number of cores (default: 1) --
 #SBATCH -n 1
 ### -- set walltime limit: j-h:m:s
-#SBATCH --time 1:0:0
+#SBATCH --time 10:0:0
 ### -- Specify the output and error file. %A_%a is the job-id --
 ### -- -o and -e mean append, -oo and -eo mean overwrite --
-#SBATCH -o /home/lfontain/unsupervised-pcn/outputs/logs/eval_PC_%A_%a.out
-#SBATCH -e /home/lfontain/unsupervised-pcn/outputs/logs/eval_PC_%A_%a.err
+#SBATCH -o /beegfs/lfontain/unsupervised-pcn/outputs/logs/eval_PC_%A_%a.out
+#SBATCH -e /beegfs/lfontain/unsupervised-pcn/outputs/logs/eval_PC_%A_%a.err
 # -- end of Slurm options --
 
 
@@ -25,4 +25,4 @@ conda activate torch_env
 
 N=(64 1280 2560 3840 5120 6400 7680 8960 10097) 
 n=${N[$SLURM_ARRAY_TASK_ID - 1]}
-srun python $HOME/unsupervised-pcn/src/pcn/eval_PC.py --N=$n
+srun python /beegfs/lfontain/unsupervised-pcn/src/pcn/eval_PC.py --N=$n
