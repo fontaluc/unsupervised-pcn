da796fcc17e936e96eed6d319450ef6b30e1b14f
Lucie Fontaine <fontaluc@gmail.com>
2025-04-04 17:57:12 +0200

    Rerun accidentally cancelled job

--- hpc/train_PC.sh
+++ hpc/train_PC.sh
@@ -5,7 +5,7 @@
 ### -- set the job Name --
 #SBATCH -J train_PC
 ### -- set the job array --
-#SBATCH --array=1-5
+###SBATCH --array=1-5
 ### -- ask for number of cores (default: 1) --
 #SBATCH -n 1
 ### -- Select the resources: 1 gpu in exclusive process mode --
@@ -14,8 +14,10 @@
 #SBATCH --time 10:0:0
 ### -- Specify the output and error file. %A_%a is the job-id --
 ### -- -o and -e mean append, -oo and -eo mean overwrite --
-#SBATCH -o /beegfs/lfontain/unsupervised-pcn/outputs/logs/train_PC_%A_%a.out
-#SBATCH -e /beegfs/lfontain/unsupervised-pcn/outputs/logs/train_PC_%A_%a.err
+###SBATCH -o /beegfs/lfontain/unsupervised-pcn/outputs/logs/train_PC_%A_%a.out
+###SBATCH -e /beegfs/lfontain/unsupervised-pcn/outputs/logs/train_PC_%A_%a.err
+#SBATCH -o /beegfs/lfontain/unsupervised-pcn/outputs/logs/train_PC_%J.out
+#SBATCH -e /beegfs/lfontain/unsupervised-pcn/outputs/logs/train_PC_%J.err
 # -- end of Slurm options --
 
 
@@ -27,8 +29,10 @@ conda activate torch_env
 
 #N=(1280 2560 3840 5120 6400 7680 8960) 
 N=(128 256 384 512 640)
-n=${N[$SLURM_ARRAY_TASK_ID - 1]}
+#n=${N[$SLURM_ARRAY_TASK_ID - 1]}
+n=${N[0]}
 #N_epochs=(3800 1900 1200 900 700 600 500)
 N_epochs=(13464 12390 11316 10243 9169)
-n_epochs=${N_epochs[$SLURM_ARRAY_TASK_ID - 1]}
+#n_epochs=${N_epochs[$SLURM_ARRAY_TASK_ID - 1]}
+n_epochs=${N_epochs[0]}
 srun python /beegfs/lfontain/unsupervised-pcn/src/pcn/train_PC.py --n_epochs=$n_epochs --N=$n
