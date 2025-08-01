af0959fbd53f3a8826ffc65fb4614872b0b51780
Lucie Fontaine <fontaluc@gmail.com>
2025-08-01 10:13:36 +0200

    Correct names

--- src/pcn/train_one_layer.py
+++ src/pcn/train_one_layer.py
@@ -83,9 +83,9 @@ def main(cf):
 
     # Evaluate validation error
     valid_error = trainer.test(valid_loader, cf.n_max_iters, cf.fixed_preds_test)
-    mode = 'a' if os.path.exists("outputs/one_layer_valid_error.txt") else 'w'
+    mode = 'a' if os.path.exists("outputs/valid_error.txt") else 'w'
     with open("outputs/valid_error.txt", mode) as f:
-        f.write(f"{cf.num_hidden}, {float(valid_error)} \n") # convert np.float64 to float for writing
+        f.write(f"{cf.n_hidden}, {float(valid_error)} \n") # convert np.float64 to float for writing
 
 if __name__ == "__main__":
 
