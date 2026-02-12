39390827f10c2ccc6ff39762ed77d59e36dccfa1
Lucie Fontaine <fontaluc@gmail.com>
2026-02-12 13:38:06 +0100

    Include positive model for evaluation

--- src/pcn/eval_one_layer.py
+++ src/pcn/eval_one_layer.py
@@ -1,6 +1,5 @@
 from pcn.models import PCModel, PCTrainer
 import torch
-from torch.utils.data import random_split
 import os
 from pcn import utils
 import argparse
@@ -18,7 +17,7 @@ def main(cf):
 
     nodes = [cf.n_vc, np.prod(size)]
 
-    model_name = f"pcn-{cf.dataset}-n_vc={cf.n_vc}" if cf.train_size == None else f"pcn-{cf.dataset}-train_size={cf.train_size}-n_vc={cf.n_vc}"
+    model_name = f"pcn-{cf.dataset}-n_vc={cf.n_vc}-positive={cf.positive}" if cf.train_size == None else f"pcn-{cf.dataset}-train_size={cf.train_size}-n_vc={cf.n_vc}-positive={cf.positive}"
     model = PCModel(
         nodes=nodes, mu_dt=cf.mu_dt, act_fn=cf.act_fn, use_bias=cf.use_bias, kaiming_init=cf.kaiming_init
     )
@@ -28,9 +27,9 @@ def main(cf):
 
     # Evaluate validation error
     valid_error = trainer.test(valid_loader, cf.n_max_iters, cf.fixed_preds_test)
-    mode = 'a' if os.path.exists(f"outputs/valid_error_{cf.dataset}.txt") else 'w'
-    with open(f"outputs/valid_error_{cf.dataset}.txt", mode) as f:
-        f.write(f"{cf.n_vc}, {valid_error} \n")
+    mode = 'a' if os.path.exists(f"outputs/valid_error_positive={cf.positive}.txt") else 'w'
+    with open(f"outputs/valid_error_positive={cf.positive}.txt", mode) as f:
+        f.write(f"{cf.dataset}, {cf.n_vc}, {valid_error} \n")
 
 if __name__ == "__main__":
 
@@ -40,6 +39,8 @@ if __name__ == "__main__":
     parser.add_argument("--dataset", choices=['mnist', 'fmnist', 'cifar10'], default='mnist', help="Enter dataset name")
     parser.add_argument("--n_vc", type=int, default=100, help="Enter size of hidden layer")
     parser.add_argument("--seed", type=int, default=0, help="Enter seed")
+    parser.add_argument("--act_fn", choices=['sigmoid', 'tanh', 'relu', 'linear'], default='sigmoid', help="Enter activation function")
+    parser.add_argument("--positive", action='store_true', help="Enable non-negative states")
     args = parser.parse_args()
 
     # Hyperparameters dict
@@ -66,7 +67,8 @@ if __name__ == "__main__":
     # model params
     cf.use_bias = True
     cf.kaiming_init = False
-    cf.act_fn = utils.Tanh()
+    cf.act_fn = args.act_fn
     cf.n_vc = args.n_vc
+    cf.positive = args.positive
 
     main(cf)
